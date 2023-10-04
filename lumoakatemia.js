254bd0a80f049bdc441cb04a36b39709da8ca804
oskarikuusikero <133215468+oskarikuusikero@users.noreply.github.com>
2023-10-04 14:49:02 +0300

    Add files via upload

---
+++ lumoakatemia.js
@@ -0,0 +1,71 @@
+ function heiMaailma {
+    console.log("Hei maailma!");
+}
+heiMaailma()
+
+function yhteenLasku(a, b){
+ console.log(a + b);
+}
+
+function erotusLasku{
+    console.log(2 - 3);
+}
+
+function tuloLasku {
+    console.log(2 * 3);
+}
+
+function jakoLasku {
+    console.log(3 / 3);
+}
+
+function celsiusFahren(celsius){
+    return celsius = 9/5 + 32;
+}
+
+
+function fahrenCelsius(fahrenheit){
+    return fahrenheit = (-32 * 1.8);
+}
+
+function mileKilo(mile){
+    return mile = mile / 1.609344;
+
+} 
+
+function kiloMile(kilo){
+    kilo = kilo / 1.609344;
+    return kilo;
+}
+
+function kiloMile(mile){
+    mile = mile * 1.609344;
+    return mile;
+}
+
+function tuumaSentti(sentti){
+    sentti = sentti * 2.54;
+    return sentti;
+}
+
+function senttiTuuma(tuuma){
+    tuuma = tuuma / 2.54;
+    return tuuma;
+}
+
+function yardMetri(yard){
+    yard = yard * 0.9144;
+    return yard;
+}
+
+function metriYard(metri){
+    metri = metri / 0.9144;
+    return metri;
+}
+
+function asciiArvo(){
+   let kirjain = .charCodeAt();
+    
+}
+
+
